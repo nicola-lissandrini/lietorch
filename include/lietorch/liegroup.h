46314b0e7513884a5a3dfc99292ddc1f3ac6a0e6
Nicola Lissandrini <nicola.lissandrini.1@gmail.com>
2022-11-17 14:51:23 +0100

    fix includes

--- include/lietorch/liegroup.h
+++ include/lietorch/liegroup.h
@@ -124,7 +124,7 @@ template<class Derived>
 LieGroup<Derived>::LieGroup(const LieGroup::DataType &_coeffs):
 	coeffs(_coeffs)
 {
-	//assert (coeffs.dim() == 1 && coeffs.size(0) == Dim && "Incompatible initialization QUAAAA tensor size");
+	//assert (coeffs.dim() == 1 && coeffs.size(0) == Dim && "Incompatible initialization tensor size");
 }
 
 template<class Derived>
